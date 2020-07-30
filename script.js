13a5df6cfdffc1848fd3b75a3646144a13b2a90f
Mike Pugach <kegach@yandex.ru>
2020-07-30 22:16:31 +0300

    переделывание 2

--- script.js
+++ script.js
@@ -15,17 +15,12 @@
 
         function formSubmitHandler (evt) {
             evt.preventDefault(); 
-            // Находим поля формы в DOM
             let modalName = document.querySelector('.popup__author');
             let modalAbout = document.querySelector('.popup__profession');
-            // Получите значение полей из свойства value
             let znach1 = modalName.value;
-            console.log(znach1);
             let znach2 = modalAbout.value;
-            // Выберите элементы, куда должны быть вставлены значения полей
             let title = document.querySelector('.profile__author');
             let subtitle = document.querySelector('.profile__profession');
-            // Вставьте новые значения с помощью textContent
             title.textContent = znach1;
             subtitle.textContent = znach2;
         }        
