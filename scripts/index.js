f160fb8e75e65b56242048ff4798bf6d4c7809e7
Mike Pugach <kegach@yandex.ru>
2020-08-03 16:47:24 +0300

    переделывание 3

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,26 @@
         let modal = document.querySelector('.popup');
         let openModalButton = document.querySelector('.profile__editbutton');
         let closeModalButton = document.querySelector('.popup__closebutton');
+        let saveModalButton = document.querySelector('.popup__savebutton');
+        let form = document.querySelector('.popup__container');
 
         function openClosePopup() {
             modal.classList.toggle('popup_opened');
             console.log('Мы открыли/закрыли окно'); 
         } 
-        openModalButton.addEventListener('click', openClosePopup);
-        closeModalButton.addEventListener('click', openClosePopup);
-
-        let saveModalButton = document.querySelector('.popup__savebutton');
-
-        let form = document.querySelector('.popup__container');
 
         function formSubmitHandler (evt) {
             evt.preventDefault(); 
             let modalName = document.querySelector('.popup__author');
             let modalAbout = document.querySelector('.popup__profession');
-            let znach1 = modalName.value;
-            let znach2 = modalAbout.value;
             let title = document.querySelector('.profile__author');
             let subtitle = document.querySelector('.profile__profession');
-            title.textContent = znach1;
-            subtitle.textContent = znach2;
-        }        
+            title.textContent = modalName.value;
+            subtitle.textContent = modalAbout.value;
+        } 
+        
+        openModalButton.addEventListener('click', openClosePopup);
+        closeModalButton.addEventListener('click', openClosePopup);
         saveModalButton.addEventListener('click', formSubmitHandler);
         form.addEventListener('submit', formSubmitHandler);
         saveModalButton.addEventListener('click',openClosePopup);
