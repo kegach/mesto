d72c04af4dfbb8a0a9ab693feec4cbb6fdcddd1a
Mike Pugach <kegach@yandex.ru>
2020-08-04 21:03:44 +0300

    проект 4

--- scripts/index.js
+++ scripts/index.js
@@ -3,18 +3,20 @@
         let closeModalButton = document.querySelector('.popup__closebutton');
         let saveModalButton = document.querySelector('.popup__savebutton');
         let form = document.querySelector('.popup__container');
-
+        let modalName = document.querySelector('.popup__author');
+        let modalAbout = document.querySelector('.popup__profession');
+        let title = document.querySelector('.profile__author');
+        let subtitle = document.querySelector('.profile__profession');
+        
         function openClosePopup() {
             modal.classList.toggle('popup_opened');
             console.log('Мы открыли/закрыли окно'); 
+            modalName.value = title.textContent;
+            modalAbout.value = subtitle.textContent;
         } 
 
         function formSubmitHandler (evt) {
             evt.preventDefault(); 
-            let modalName = document.querySelector('.popup__author');
-            let modalAbout = document.querySelector('.popup__profession');
-            let title = document.querySelector('.profile__author');
-            let subtitle = document.querySelector('.profile__profession');
             title.textContent = modalName.value;
             subtitle.textContent = modalAbout.value;
         } 
